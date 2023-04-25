acfbe22be44e3317c1b1fbef3c3fbe3e874df8be
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-04-25 20:33:55 -0300

    Finalizando estrutura e adicionando correntista

---
+++ AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
@@ -0,0 +1,22 @@
+﻿using AppBancoDigital.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppBancoDigital.Service
+{
+    public class DataServiceCorrentista : DataService
+    {
+        public static async Task<Correntista> Cadastrar(Correntista c)
+        {
+            var json_a_enviar = JsonConvert.SerializeObject(c);
+
+            string json = await DataService.PostDataToService(json_a_enviar, "/correntista/salvar");
+
+            Correntista co = JsonConvert.DeserializeObject<Correntista>(json);
+
+            return co;
+        }
+    }
+}
