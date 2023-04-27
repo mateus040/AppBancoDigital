6a33e6a1c9181105c0ad5f7b09b3ab4f26f50c86
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-04-27 14:45:46 -0300

    criando View de correntista

---
+++ AppBancoDigital/AppBancoDigital/View/FormAdd.xaml.cs
@@ -0,0 +1,54 @@
+﻿using AppBancoDigital.Model;
+using AppBancoDigital.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormAdd : ContentPage
+    {
+        public FormAdd()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            act_carregando.IsRunning = true;
+            act_carregando.IsVisible = true;
+
+            try
+            {
+                Correntista co = await DataServiceCorrentista.Cadastrar(new Correntista
+                {
+                    Nome = txt_nome.Text,
+                    Cpf = int.Parse(txt_cpf.Text),
+                    Data_Nasc = dtpck_data_nasc.Date,
+                    Senha = txt_senha.Text                 
+                });
+
+                string msg = $"Correntista inserido com sucesso. Código gerado: {co.Id} ";
+
+                await DisplayAlert("Sucesso!", msg, "OK");
+
+                await Navigation.PushAsync(new Listagem());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+            finally
+            {
+                act_carregando.IsRunning = false;
+                act_carregando.IsVisible = false;
+            }
+        }
+    }
+}
