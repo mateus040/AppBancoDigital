b824ab153dfe5faded108399dfa33e480075d8f8
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-04-27 15:40:54 -0300

    finalizando arquivo DataServiceCorrentista

--- AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
+++ AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
@@ -8,6 +8,16 @@ namespace AppBancoDigital.Service
 {
     public class DataServiceCorrentista : DataService
     {
+        public static async Task<List<Correntista>> GetCorrentistasAsync()
+        {
+            string json = await DataService.GetDataFromService("/correntista");
+
+            List<Correntista> arr_corretistas = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return arr_corretistas;
+        }
+
+
         public static async Task<Correntista> Cadastrar(Correntista c)
         {
             var json_a_enviar = JsonConvert.SerializeObject(c);
@@ -17,6 +27,28 @@ namespace AppBancoDigital.Service
             Correntista co = JsonConvert.DeserializeObject<Correntista>(json);
 
             return co;
+        }
+
+        public static async Task<List<Correntista>> SearchAsync(string q)
+        {
+            var json_a_enviar = JsonConvert.SerializeObject(q);
+
+            string json = await DataService.PostDataToService(json_a_enviar, "/correntista/buscar");
+
+            List<Correntista> arr_correntistas = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return arr_correntistas;
+        }
+
+        public static async Task<List<Correntista>> DeleteAsync(int id)
+        {
+            var json_a_enviar = JsonConvert.SerializeObject(id);
+
+            string json = await DataService.PostDataToService(json_a_enviar, "/correntista/delete");
+
+            List<Correntista> arr_correntistas = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return arr_correntistas;
         }
     }
 }
