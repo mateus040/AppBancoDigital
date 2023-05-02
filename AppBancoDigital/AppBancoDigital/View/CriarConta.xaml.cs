f998e4589742ebaf27f1ccaf75207b2d10fb84ca
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-05-02 15:54:17 -0300

    criando tela de CriarConta

---
+++ AppBancoDigital/AppBancoDigital/View/CriarConta.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CriarConta : ContentPage
+    {
+        public CriarConta()
+        {
+            InitializeComponent();
+
+            logo.Source = ImageSource.FromResource("AppBancoDigital.View.Img.logo.png");
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void btn_CriarConta_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Login());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message, "OK");
+            }
+        }
+    }
+}
