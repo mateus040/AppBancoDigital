647e3e590c9aa623ac5087ec3b7a180a76d94414
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-05-09 15:52:52 -0300

    adicionando configuração login

--- AppBancoDigital/AppBancoDigital/App.xaml.cs
+++ AppBancoDigital/AppBancoDigital/App.xaml.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+using AppBancoDigital.View;
+using AppBancoDigital.Model;
 
 namespace AppBancoDigital
 {
     public partial class App : Application
     {
+        public List<DadosUsuario> list_usuarios = new List<DadosUsuario>
+        {
+            new DadosUsuario()
+            {
+                Cpf = "561.822.558-11",
+                Senha = "123"
+            }
+        };
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new View.Login());
+            if (Properties.ContainsKey("usuario_logado"))
+                MainPage = new FormAdd();
+            else
+                MainPage = new Login();
         }
 
         protected override void OnStart()
---
+++ AppBancoDigital/AppBancoDigital/Model/DadosUsuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Model
+{
+    public class DadosUsuario
+    {
+        public string Cpf { get; set; }
+        public string Senha { get; set; }
+    }
+}
