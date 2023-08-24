05255c1326199c195ff6b904f3614adaea1e4c0f
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-08-24 14:07:38 -0300

    feat: construindo estrutura para pix

---
+++ AppBancoDigital/AppBancoDigital/View/Pix/EnviarPix.xaml.cs
@@ -0,0 +1,34 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View.Pix
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnviarPix : ContentPage
+    {
+        public EnviarPix()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string teste = "Chave de Transferência: mateusgabrielmoreno264@gmail.com";
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(teste, QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+            byte[] qrCodeBytes = qrCode.GetGraphic(20);
+            img_qrcode.Source = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
+        }
+    }
+}
---
+++ AppBancoDigital/AppBancoDigital/View/Pix/PixLerQrCode.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View.Pix
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PixLerQrCode : ContentPage
+    {
+        public PixLerQrCode()
+        {
+            InitializeComponent();
+
+            zxing.OnScanResult += (result) =>
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    lblResult.Text = result.Text;
+                });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxing.IsScanning = false;
+
+            base.OnDisappearing();
+        }
+    }
+}
